036c4095161baec731d9989d399c0955bc3f16fe
Silvano Amaral <silvano.amaral9@gmail.com>
2018-05-11 20:38:02 -0300

    ajuste no metodo soma quando informa parametro

--- test/soma.spec.js
+++ test/soma.spec.js
@@ -1,10 +1,15 @@
-const { expect } = require('code');
 const Lab = require('lab');
 const lab = exports.lab = Lab.script();
+const { expect } = require('code');
 
 const { soma } = require('../soma');
 
 lab.test('returns true when 1 + 1 equals 2', () => {
     expect(soma(1,1)).to.equal(2);
+    //done();
+});
+
+lab.test('rodar sem parametros', () => {
+    expect(soma()).to.equal(0);
     //done();
 });
