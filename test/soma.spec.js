0e22d82e746d99b67af9ccf7985243d3a381ea63
Silvano Amaral <silvano.amaral9@gmail.com>
2018-05-11 20:26:38 -0300

    ajuste no teste

--- test/soma.spec.js
+++ test/soma.spec.js
@@ -2,7 +2,9 @@ const { expect } = require('code');
 const Lab = require('lab');
 const lab = exports.lab = Lab.script();
 
+const { soma } = require('../soma');
+
 lab.test('returns true when 1 + 1 equals 2', () => {
-    expect(soma(1 + 1)).to.equal(2);
-    done();
+    expect(soma(1,1)).to.equal(2);
+    //done();
 });
